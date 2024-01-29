6de15dee530b3d456fa2226a229ed4a647b87acd
hunghx <baby10051999@gmail.com>
2024-01-29 19:17:20 +0700

    update

---
+++ src/main/java/ra/projectmd3/config/AppInit.java
@@ -0,0 +1,32 @@
+package ra.projectmd3.config;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    // cấu hin utf-8
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setForceEncoding(true);
+        filter.setEncoding("utf-8");
+        return new Filter[]{filter};
+    }
+}
---
+++ src/main/java/ra/projectmd3/model/Employee.java
@@ -0,0 +1,18 @@
+package ra.projectmd3.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Employee {
+    @Id
+    private int id;
+    private String name;
+}
